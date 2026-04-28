f154bca6f44e09548c410f934485fadd34f331e7
qintianxiang <qintianxiang@meituan.com>
2026-04-28 23:09:16 +0800

    feat: 支持 ios 下的 wifi 切换

--- ios/Classes/QuickTransferWifiConnectorPlugin.m
+++ ios/Classes/QuickTransferWifiConnectorPlugin.m
@@ -3,6 +3,19 @@
 #import <NetworkExtension/NetworkExtension.h>
 #import <UIKit/UIKit.h>
 
+@interface QuickTransferWifiConnectorPlugin ()
+
+@property(nonatomic, copy, nullable) NSString *previousSSID;
+@property(nonatomic, copy, nullable) NSString *deviceSSID;
+
+- (void)prepareForDeviceWifiTransition:(FlutterMethodCall *)call result:(FlutterResult)result;
+- (void)joinWifiNetwork:(FlutterMethodCall *)call result:(FlutterResult)result;
+- (void)restorePreviousNetwork:(FlutterMethodCall *)call result:(FlutterResult)result;
+- (void)openWifiSettingsWithResult:(FlutterResult)result;
+- (void)capturePreviousSSIDExcluding:(NSString *)deviceSSID;
+
+@end
+
 @implementation QuickTransferWifiConnectorPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
@@ -13,8 +26,16 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if ([call.method isEqualToString:@"prepareForDeviceWifiTransition"]) {
+        [self prepareForDeviceWifiTransition:call result:result];
+        return;
+    }
     if ([call.method isEqualToString:@"joinWifiNetwork"]) {
         [self joinWifiNetwork:call result:result];
+        return;
+    }
+    if ([call.method isEqualToString:@"restorePreviousNetwork"]) {
+        [self restorePreviousNetwork:call result:result];
         return;
     }
     if ([call.method isEqualToString:@"openWifiSettings"]) {
@@ -22,6 +43,20 @@
         return;
     }
     result(FlutterMethodNotImplemented);
+}
+
+- (void)prepareForDeviceWifiTransition:(FlutterMethodCall *)call result:(FlutterResult)result {
+    NSDictionary *arguments = [call.arguments isKindOfClass:[NSDictionary class]] ? call.arguments : nil;
+    if (!arguments) {
+        result([FlutterError errorWithCode:@"INVALID_ARGUMENT" message:@"Arguments must be a map." details:nil]);
+        return;
+    }
+
+    NSString *rawSsid = [arguments[@"deviceSsid"] isKindOfClass:[NSString class]] ? arguments[@"deviceSsid"] : @"";
+    NSString *ssid = [rawSsid stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    [self capturePreviousSSIDExcluding:ssid];
+    self.deviceSSID = ssid;
+    result(nil);
 }
 
 - (void)joinWifiNetwork:(FlutterMethodCall *)call result:(FlutterResult)result {
@@ -47,6 +82,9 @@
                                    details:nil]);
         return;
     }
+
+    [self capturePreviousSSIDExcluding:ssid];
+    self.deviceSSID = ssid;
 
     if (@available(iOS 11.0, *)) {
         NEHotspotConfiguration *configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid passphrase:password isWEP:NO];
@@ -105,6 +143,57 @@
     [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL opened) {
         result(@(opened));
     }];
+}
+
+- (void)restorePreviousNetwork:(FlutterMethodCall *)call result:(FlutterResult)result {
+    NSDictionary *arguments = [call.arguments isKindOfClass:[NSDictionary class]] ? call.arguments : nil;
+    if (!arguments) {
+        result([FlutterError errorWithCode:@"INVALID_ARGUMENT" message:@"Arguments must be a map." details:nil]);
+        return;
+    }
+
+    NSString *fallbackSsid = self.deviceSSID ?: @"";
+    NSString *rawSsid = [arguments[@"deviceSsid"] isKindOfClass:[NSString class]] ? arguments[@"deviceSsid"] : fallbackSsid;
+    NSString *ssid = [rawSsid stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+    if (@available(iOS 11.0, *)) {
+        if (ssid.length > 0) {
+            [[NEHotspotConfigurationManager sharedManager] removeConfigurationForSSID:ssid];
+        }
+
+        NSString *message = self.previousSSID.length > 0
+            ? [NSString stringWithFormat:@"已移除设备热点配置，系统将尝试恢复到之前的网络：%@", self.previousSSID]
+            : @"已移除设备热点配置，系统将尝试恢复默认网络。";
+        result(@{
+            @"status": @"best_effort",
+            @"message": message,
+            @"platform": @"ios"
+        });
+        return;
+    }
+
+    result(@{
+        @"status": @"unavailable",
+        @"message": @"iOS 11.0 or later is required.",
+        @"platform": @"ios"
+    });
+}
+
+- (void)capturePreviousSSIDExcluding:(NSString *)deviceSSID {
+    if (@available(iOS 14.0, *)) {
+        [NEHotspotNetwork fetchCurrentWithCompletionHandler:^(NEHotspotNetwork * _Nullable currentNetwork) {
+            if (!currentNetwork) {
+                return;
+            }
+
+            NSString *ssid = [currentNetwork.SSID stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            if (ssid.length == 0 || [ssid isEqualToString:deviceSSID]) {
+                return;
+            }
+
+            self.previousSSID = ssid;
+        }];
+    }
 }
 
 @end
